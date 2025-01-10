fc6dad10b30ad3d079aeb45f6b94840f5c515cb9
code2811 <f.kayiranga@alustudent.com>
2025-01-10 04:36:14 -0800

    8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-
-log_entry = ARGV[0]
-
-# Regular expression to extract the required parts from the log entry
-regex = /\[from:([^\]]+)\].*\[to:([^\]]+)\].*\[flags:([^\]]+)\]/
-
-# Match the regex and extract the parts
-match = log_entry.match(regex)
-
-# Output the result if a match is found
-if match
-  puts "#{match[1]},#{match[2]},#{match[3]}"
-end
-````
+puts ARGV[0].scan(/\[from:(.?)\] \[to:(.?)\] \[flags:(.*?)\]/).map { |match| match.join(',') }.join
