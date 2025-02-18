8c0ec6cf4b72f7ab3d8bbac485535d7ce34bf583
code2811 <f.kayiranga@alustudent.com>
2025-02-18 01:25:03 -0800

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -5,13 +5,13 @@
 import sys
 
 if __name__ == '__main__':
-    # Import the count_words function from the 3-count.py file
-    count_words = __import__('api_advanced.3-count').count_words  # Adjusted to use api_advanced module
+    count_words = __import__('api_advanced.3-count').count_words  # Import count_words from 3-count.py
     
+    # Ensure proper usage of the script
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
         print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
     else:
-        # Call the count_words function with the subreddit and list of keywords
+        # Call the function with the subreddit and word list
         result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
 
