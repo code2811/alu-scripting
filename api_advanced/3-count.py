f7856e271557f9417c2752a86ebd57ed8f2672a8
code2811 <f.kayiranga@alustudent.com>
2025-02-21 22:07:06 +0000

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""
-3-main
-"""
-import sys
+"""Exports data in the JSON format"""
 
-if __name__ == '__main__':
-    count_words = __import__('api_advanced.3-count').count_words  # Import count_words from 3-count.py
-    
-    # Ensure proper usage of the script
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-    else:
-        # Call the function with the subreddit and word list
-        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
+if __name__ == "__main__":
 
+    import json
+    import requests
+    import sys
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
