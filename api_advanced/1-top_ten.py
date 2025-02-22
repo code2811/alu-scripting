14aa6a49043a275e64ade1c7f4e15100a57e3d1e
code2811 <f.kayiranga@alustudent.com>
2025-02-22 18:21:42 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,23 +2,14 @@
 """
 This module contains a function that queries the Reddit API
 and prints the titles of the first 10 hot posts for a given subreddit.
-
-Functions:
-    top_ten(subreddit): Prints titles of top 10 hot posts in a subreddit
 """
 import requests
 
 
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query
-
-    Returns:
-        None
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints None.
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
@@ -36,23 +27,12 @@ def top_ten(subreddit):
             allow_redirects=False
         )
 
-        if response.status_code == 404:
-            print("OK")
-            return
-
         if response.status_code == 200:
-            results = response.json().get("data", {}).get("children", [])
-            
-            if not results:
-                print("OK")
-                return
-
-            # Print first 10 hot posts
-            for post in results[0:10]:
-                print(post.get("data", {}).get("title"))
-            print("OK")
+            data = response.json()
+            for post in data["data"]["children"]:
+                print(post["data"]["title"])
         else:
-            print("OK")
+            print("None")
 
     except Exception:
-        print("OK")
+        print("None")
